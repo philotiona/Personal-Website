7c2e9af8cadeaa43d1f87970d6876d1fd649a894
truthofgod <123151544+truthofgod@users.noreply.github.com>
2025-08-12 23:52:35 +0500

    feat: Header Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import type { ReactNode } from "react";
+import Header from "./components/Header/Header";
 
 export default function App(): ReactNode {
   return(
     <main>
-      
+      <Header/>
     </main>
   ) 
 } 
