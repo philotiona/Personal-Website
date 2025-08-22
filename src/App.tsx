0266f32eb1dca22c855e495085177df87bd15f09
truthofgod <123151544+truthofgod@users.noreply.github.com>
2025-08-22 22:26:23 +0500

    feat: About Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { type ReactNode } from "react";
 import Hero from "./components/Hero/Hero";
 import ProjectList from "./components/ProjectList/ProjectList";
+import About from "./components/About/About";
 
 export default function App(): ReactNode {
   return(
     <div className=" flex relative">
       <div className="h-screen w-full opacity-50">
-        <img src="sky.jpg" alt="Sky" className="w-full h-screen"/>
+        <img src="sky.jpg" alt="Sky" className="w-full h-screen "/>
       </div>
       <div className="absolute w-1/5 h-2/5 opacity-50">
         <img src="clou.png" alt="" />
@@ -17,6 +18,7 @@ export default function App(): ReactNode {
       <main className="bg-[var(--blue)] absolute w-full">        
         <Hero/>
         <ProjectList/>
+        <About/>
       </main>
     </div>
   ) 
