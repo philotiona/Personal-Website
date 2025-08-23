e75e6ebfa1b3b3dcb396d9ad4c232f7eb7c47cd5
truthofgod <123151544+truthofgod@users.noreply.github.com>
2025-08-23 20:35:31 +0500

    fix: Small Fixes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export default function About(): ReactNode {
     return(
-        <section className="bg-[var(--color-darkblue)] w-full h-screen flex flex-row flex-wrap">
+        <section className="bg-[var(--color-ultramarine)] w-full h-screen flex flex-row flex-wrap">
             About
         </section>
     )
